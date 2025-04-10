d12fc612bbd10948bf4ffb35124f1aeb00dc4b1f
Murakami <murakami.edson@gmail.com>
2025-04-10 08:02:21 -0300

    Versao estavel depois do server-json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { UsuComponent } from './usu/usu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UsuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
